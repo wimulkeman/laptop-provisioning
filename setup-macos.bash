79701d8c6e108307df0528a4bf95d88df3e1a628
Wim Ulkeman <wimulkeman@hotmail.com>
2024-10-20 17:20:35 +0200

    chore(setup): remove custom brew install commands

--- setup-macos.bash
+++ setup-macos.bash
@@ -17,20 +17,3 @@ fi
 
 curl -fsSL https://raw.githubusercontent.com/wimulkeman/laptop-provisioning/refs/heads/main/darwin/Brewfile > Brewfile
 brew bundle install --file=./Brewfile
-
-#echo "==> Checking Homebrew packages"
-#for package in "git" "ansible"
-#do
-#  packageAvailable=`command -v $package`
-#  if [ "${packageAvailable}" = "" ]; then
-#    echo "## $package not available"
-#    echo "++ Installing $package"
-#    brew install $package
-#  else
-#    echo "## $package already installed"
-#  fi
-#done
-#
-#echo "## Installed Ansible versions"
-#ansible --version
-#ansible-community --version
