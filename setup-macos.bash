3c5bd96c1a59574de852de1ba6e18853098fa19e
Wim Ulkeman <wimulkeman@hotmail.com>
2024-10-20 18:20:55 +0200

    chore(setup): move filevault
    Moving the FileVault enabling to the top to ensure its correct before installing more files.

--- setup-macos.bash
+++ setup-macos.bash
@@ -4,8 +4,10 @@ echo "========================================"
 echo "          Laptop Provisioning           "
 echo "========================================"
 
-echo "==> Checking availability Brew"
+echo '==> Enabling FileVault'
+sudo fdesetup enable
 
+echo "==> Checking availability Brew"
 brewAvailable=`command -v brew`
 if [ "${brewAvailable}" = "" ]; then
   echo "## Brew not available"
@@ -19,6 +21,3 @@ echo "==> Fetching Brewfile"
 curl -fsSL https://raw.githubusercontent.com/wimulkeman/laptop-provisioning/refs/heads/main/darwin/Brewfile > Brewfile
 echo "==> Installing dependencies through Brew"
 brew bundle install --file=./Brewfile
-
-echo '==> Enabling FileVault'
-sudo fdesetup enable
